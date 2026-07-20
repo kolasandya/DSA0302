679b9a41741b1ec7db13dc0a11b3f2c0872bd553
kolasandya <kolasandhyamurali@gmail.com>
2026-07-20 11:58:34 +0530

    Add files via upload

---
+++ ASSESSMENT/CO1/AT1/exp 2 output.py
@@ -0,0 +1,12 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: C:/Users/K sandhya/OneDrive/Documents/exp 2.py ===========
+Enter product keyword: 
+============ RESTART: C:/Users/K sandhya/OneDrive/Documents/exp 2.py ===========
+Enter product keyword: laptop
+Matching Products:
+Laptop
+Laptop Bag
+Gaming Laptop
+Total Matches: 3
