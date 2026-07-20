2e6af63a72008769cf8eac9fd84595acd1615bd0
kolasandya <kolasandhyamurali@gmail.com>
2026-07-20 11:55:28 +0530

    Add files via upload

---
+++ ASSESSMENT/CO1/AT1/exp1 output.py
@@ -0,0 +1,10 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: C:/Users/K sandhya/OneDrive/Documents/EXP 1.PY ===========
+Name: Sandhya
+Email: kolasandhyamurali@gmail.com
+Phone: 9063724029
+Skills: ['Python', 'SQL', 'Machine Learning']
+Experience: 3 years
+Eligible Candidate
