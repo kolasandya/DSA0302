b8c8530b8a7fc8196851c8f3ca9e80a13734a935
kolasandya <kolasandhyamurali@gmail.com>
2026-07-29 08:31:17 +0530

    Add files via upload

---
+++ ASSESSMENT/CO1/AT1/exp 3 output.py
@@ -0,0 +1,10 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: C:/Users/K sandhya/OneDrive/Documents/exp 3.py ===========
+Register Number: 192472207
+Email: kolasandhyamurali@gmail.com
+Course Code: dsa0302
+Semester: 3
+Mobile Number: 9063724029
+Registration Failed
